9ba9e69014ac4ad7c51739544c7411a17406d8dc
tech-genius <adebimpehabeeb10@gmail.com>
2023-06-20 08:38:26 -0700

    lo

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,13 @@
 #include "main.h"
 #include "6-abs.c"
 #include <stdio.h>
-
+/**
+ * print_to_98 - Prints all numbers from n to 98
+ * @n: The starting number
+ *
+ * Description: This function prints all the numbers from the given starting
+ * number (n) up to 98, inclusive, in ascending order.
+ */
 void print_to_98(int n)
 {
 	int i;
